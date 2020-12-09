c478c932f64e194bc4120e6b5f260bcacd454e40
Madeleine Boyd <60855502+maddylilo@users.noreply.github.com>
2020-12-09 17:35:11 +0000

    JS added to celsius and fahrenheit links to convert temperature

--- sources/script.js
+++ sources/script.js
@@ -1,63 +1,3 @@
-// Display Current Time and Date
-
-//let now = new Date();
-//let currentHour = now.getHours();
-//let currentMinutes = now.getMinutes();
-//let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//let currentDay = daysOfWeek[now.getDay()];
-
-//let dateTime = document.querySelector("#currentDayTime");
-//dateTime.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
-
-
-// Week 5 HOMEWORK
-
-//function showPlaceTemp(response) {
-  //let h1 = document.querySelector("h1");
-  //h1.innerHTML = response.data.name;
-  //let temperatureElement = document.querySelector("#temperature");
-  //temperatureElement.innerHTML = Math.round(response.data.main.temp);
-//}
-
-//function searchCity(cityEntry) {
-  //let apiKey = "34d9ed3eeaae24ff1bdbb0e4f7d779cd";
-  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityEntry}&appid=${apiKey}&units=metric`;
-  //axios.get(apiUrl).then(showPlaceTemp);
-//}
-
-//function submitCity(event) {
-  //event.preventDefault();
-  //let cityEntry = document.querySelector("#search-bar").value;
-  //searchCity(cityEntry);
-//}
-
-//let searchBarEntry = document.querySelector("#search-form");
-//searchBarEntry.addEventListener("submit", submitCity);
-
-
-
-// Display (fake) temperature and link to convert between C and F
-
-//function displayCelsius(event){
-  //event.preventDefault();
-  //let currentTemperature = document.querySelector("#temperatureNow");
-  //currentTemperature.innerHTML = 12;
-//}
-
-//UNIT BUTTON C
-//let celsiusButton = document.querySelector("#celsius");
-//celsiusButton.addEventListener("click", displayCelsius);
-
-//function displayFahrenheit(event){
-  //event.preventDefault();
-  //let currentTemperature = document.querySelector("#temperatureNow");
-  //currentTemperature.innerHTML = 54;
-//}
-
-//UNIT BUTTON F
-//let fahrenheitButton = document.querySelector("#fahrenheit");
-//fahrenheitButton.addEventListener("click", displayFahrenheit);
-
 let now = new Date();
 let currentHour = now.getHours();
 let currentMinutes = now.getMinutes();
@@ -76,7 +16,9 @@ function displayCurrentTemp(response){
   let humidityElement = document.querySelector("#humidity");
   let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -97,7 +39,28 @@ function handleSubmit(event) {
   search(citySearchInput.value);
 }
 
-search("London");
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature*9)/5+32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsius);
+
+search("London");
